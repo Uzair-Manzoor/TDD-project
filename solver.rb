5476de9d316632265d6d201cd3c7221ddc451d8f
Enoh Collins <eno.col@gmail.com>
2023-11-24 13:57:37 +0000

    Edit solver.rb file

--- solver.rb
+++ solver.rb
@@ -1,9 +1,13 @@
 class Solver
-  def self.factorial(n)
-      raise ArgumentError, 'Input must be a non-negative integer' if n < 0
+    def self.factorial(n)
+        raise ArgumentError, 'Input must be a non-negative integer' if n < 0
+    
+        result = 1
+        (1..n).each { |i| result *= i }
+        result
+    end
 
-      result = 1
-      (1..n).each { |i| result *= i }
-      result
-  end
+    def self.reverse(word)
+        word.reverse
+    end
 end
